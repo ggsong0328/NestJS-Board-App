281ea95b03543db868cdecdf11be3a38ec25488e
ggsong0328 <ggsong0328@gmail.com>
2025-05-21 02:48:51 +0900

    [NEST]05.21

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; //데이터베이스 사용 X
+
+  //모든 게시판 가져오기
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  //게시판 생성
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+
+  //특정 ID로 게시물 가져오기
+  getBoardById(id: string): Board | undefined {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  //ID로 특정 게시물 지우기
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  //특정 게시물 상태 업데이트
+  updateBoardStatus(id: string, status: BoardStatus): Board | undefined {
+    const board = this.getBoardById(id);
+    board!.status = status;
+    return board;
+  }
+}
