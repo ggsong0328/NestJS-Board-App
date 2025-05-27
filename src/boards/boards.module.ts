e3ae45898142fef81f0a81c702cd362dd600e7b3
ggsong0328 <ggsong0328@gmail.com>
2025-05-27 10:49:44 +0900

    [NEST]Section 7

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,9 +4,10 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { TypeOrmExModule } from '../typeorm-ex/typeorm-ex.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
